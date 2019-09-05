a35f3415f89bdc1c7008eee996e34c9d77271680
Bruno Zanholo <bruno@zanholo.com.br>
2019-09-05 14:10:03 -0300

    Update sql.sql
    SQL

--- sql.sql
+++ sql.sql
@@ -3,10 +3,56 @@ dbserver-tcc.database.windows.net / tcc
 tcc / puc@2019
 */
 
+DROP TABLE SENSORES
 CREATE TABLE DBO.SENSORES
 (
 	SensorId int not null identity (1,1),
+	AreaId int not null,
 	Rotulo varchar(100) not null,
 	Nome varchar(100) not null,
 )
 
+DROP TABLE AFETADOS
+CREATE TABLE DBO.AFETADOS
+(
+	AfetadoId int not null identity (1,1),
+	AreaId int not null,
+	Email varchar(255) not null,
+	Celular varchar(20) not null,
+)
+
+DROP TABLE AREAS
+CREATE TABLE DBO.AREAS
+(
+	AreaId int not null identity (1,1),
+	Nome varchar(100) not null,
+)
+
+DROP TABLE ATIVIDADES
+CREATE TABLE DBO.ATIVIDADES
+(
+	AtividadeId int not null identity (1,1),
+	RotuloSensor varchar(100) not null,
+	Nome varchar(100) not null,
+	Tipo varchar(100) not null,
+	Intensidade int not null,
+)
+
+DROP TABLE INCIDENTES
+CREATE TABLE DBO.INCIDENTES
+(
+	IncidenteId int not null identity (1,1),
+	AreaId int not null identity (1,1),
+	Data Datetime not null default ((getdate())),
+	Nome varchar(100) not null,
+	PlanoAcaoId int not null ,
+	Classificacao int not null,
+)
+
+DROP TABLE PLANOSACAO
+CREATE TABLE DBO.PLANOSACAO
+(
+	PlanoAcaoId int not null identity (1,1),
+	Tipo varchar(100) not null,
+	Classificacao int not null,
+)
