311ccbd81fe845bb845e5b3fa06e06a3d5bb43a6
Bruno Zanholo <bruno@zanholo.com.br>
2019-09-08 09:10:03 -0300

    asd

--- TCC.IdentityServer/Startup.cs
+++ TCC.IdentityServer/Startup.cs
@@ -2,27 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace TCC.IdentityServer
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, ILoggerFactory loggerFactory)
         {
             Configuration = configuration;
+            LoggerFactory = loggerFactory;
         }
 
         public IConfiguration Configuration { get; }
 
+        public ILoggerFactory LoggerFactory { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var cors = new DefaultCorsPolicyService(LoggerFactory.CreateLogger<DefaultCorsPolicyService>())
+            {
+                AllowAll = true
+            };
+            services.AddSingleton<ICorsPolicyService>(cors);
+
             services.AddIdentityServer()
                         .AddDeveloperSigningCredential(filename: "tempkey.rsa")
                         .AddInMemoryApiResources(Config.GetApiResources())
@@ -36,6 +47,12 @@ namespace TCC.IdentityServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            if (!env.IsDevelopment())
+            {
+                app.UseHsts();
+                app.UseHttpsRedirection();
+            }
+
             app.UseDeveloperExceptionPage();
             app.UseIdentityServer();
             app.UseStaticFiles();
